342d3a1774819ba4ee3b9223d4bf400026e8798d
Tore André Klock <alphapenguin73@gmail.com>
2022-04-29 21:37:54 -0400

    Merged with remote

---
+++ index.ts
@@ -0,0 +1,29 @@
+// Require the necessary discord.js classes
+import { Client, Intents } from "discord.js";
+import { token, clientId, guildId } from "./config.json";
+import * as wowApi from "./wowapi";
+
+const wowToMarkdown = (wow: wowApi.Wow) =>
+  `${wow.video["1080p"]}\n> ${wow.full_line}\n> \u2015 "${wow.character}" at ${wow.timestamp} in "${wow.movie}" (${wow.year}) directed by ${wow.director}\n(#${wow.current_wow_in_movie} of ${wow.total_wows_in_movie} wows in the movie)`;
+
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+// When the client is ready, run this code (only once)
+client.once("ready", () => {
+  console.log("Ready!");
+});
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isCommand()) return;
+
+  const { commandName } = interaction;
+
+  if (commandName === "wow") {
+    const wows = await wowApi.getRandom();
+    await interaction.reply(wowToMarkdown(wows[0]));
+  }
+});
+
+// Login to Discord with your client's token
+client.login(token);
---
+++ wowapi.ts
@@ -0,0 +1,28 @@
+import fetch from "node-fetch";
+
+const wowApiUrl = "https://owen-wilson-wow-api.herokuapp.com/wows";
+
+export type Wow = {
+  movie: string;
+  year: number;
+  release_date: string;
+  director: string;
+  character: string;
+  full_line: string;
+  timestamp: string;
+  current_wow_in_movie: number;
+  total_wows_in_movie: number;
+  poster: string;
+  video: {
+    [format: string]: string;
+  };
+  audio: string;
+};
+
+export type WowApiResponse = Wow[];
+
+export async function getRandom(): Promise<WowApiResponse> {
+  return fetch(`${wowApiUrl}/random`)
+    .then((r) => r.json())
+    .then((j) => j as WowApiResponse);
+}
